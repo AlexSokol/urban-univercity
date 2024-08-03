48b9d9c25c058d4d426a873db1020d7e20c53c1e
Alexander Sokolov <alexanderysokoloff@gmail.com>
2024-08-03 17:39:33 +0800

    Homework 3

---
+++ Module 1/Homework3.py
@@ -0,0 +1,14 @@
+print('Input string: ')
+my_string = input()
+print(len(my_string))
+
+#Выведите строку my_string в верхнем регистре.
+print(my_string.upper())
+#Выведите строку my_string в верхнем регистре.
+print(my_string.lower())
+#Измените строку my_string, удалив все пробелы.
+print(my_string.replace(' ', ''))
+#Выведите первый символ строки my_string.
+print(my_string[0])
+#Выведите последний символ строки my_string.
+print(my_string[-1])
