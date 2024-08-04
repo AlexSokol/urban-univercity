8faa6e31926fb98404a05c2c1ec13819d9719346
Alexander Sokolov <alexanderysokoloff@gmail.com>
2024-08-04 12:38:03 +0800

    Homework 5

---
+++ Module 1/Homework5.py
@@ -0,0 +1,15 @@
+#Работа со списками:
+my_list = ['orange', 'banana', 'apple', 'pineapple', 'coconut']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'peach'
+print(my_list)
+
+#Работа со словарями:
+my_dict = {'orange': 'апельсин', 'banana': 'банан', 'apple': 'яблоко', 'pineapple': 'ананас', 'coconut': 'кокосик'}
+print(my_dict)
+print(my_dict.get('banana'))
+my_dict['coconut'] = 'кокос'
+print(my_dict)
